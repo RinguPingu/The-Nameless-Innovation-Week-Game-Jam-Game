09c18b8831abef2edd8d87ba9b9e50233cad6c86
Hugo <hugo.ratiney@gmail.com>
2021-08-27 10:55:55 +0200

    Open doors + stuff
    asdf

--- Assets/Scripts/EnemyFollowAvoid.cs
+++ Assets/Scripts/EnemyFollowAvoid.cs
@@ -14,6 +14,18 @@ public class EnemyFollowAvoid : MonoBehaviour
     [Tooltip("0 = follow, 180 = avoid")]
     float followAngle = 0f;
 
+    [SerializeField]
+    [Tooltip("0 = follow, 180 = avoid")]
+    float brakeForce = 10f;
+
+    [SerializeField]
+    [Range(0f, 25f)]
+    float minDistance = 0f;
+
+    [SerializeField]
+    [Range(0f, 25f)]
+    float maxDistance = 25f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,32 +44,42 @@ public class EnemyFollowAvoid : MonoBehaviour
         var dist = delta.magnitude;
         var dir = delta / dist;
 
-        var raycastHit = Physics2D.Raycast(transform.position, dir, dist, ~(1 << LayerMask.NameToLayer("Enemy")));
+        bool follow = true;
 
-        var perp = Vector3.Cross(dir, Vector3.forward) * .2f;
-        var raycastHit2 = Physics2D.Raycast(transform.position, (mouseWorld + perp - transform.position).normalized, dist, ~(1 << LayerMask.NameToLayer("Enemy")));
-        var raycastHit3 = Physics2D.Raycast(transform.position, (mouseWorld - perp - transform.position).normalized, dist, ~(1 << LayerMask.NameToLayer("Enemy")));
+        if (followIfVisible)
+        {
+            var layerMask = ~(1 << LayerMask.NameToLayer("Enemy"));
 
-        bool visible = !raycastHit.collider || !raycastHit2.collider || !raycastHit3.collider;
+            var raycastHit = Physics2D.Raycast(transform.position, dir, dist, layerMask);
 
+            var perp = Vector3.Cross(dir, Vector3.forward) * .2f;
+            var raycastHit2 = Physics2D.Raycast(transform.position, (mouseWorld + perp - transform.position).normalized, dist, layerMask);
+            var raycastHit3 = Physics2D.Raycast(transform.position, (mouseWorld - perp - transform.position).normalized, dist, layerMask);
 
-        Debug.DrawLine(transform.position, mouseWorld);
-        Debug.DrawLine(transform.position, mouseWorld + perp);
-        Debug.DrawLine(transform.position, mouseWorld - perp);
+            follow = !raycastHit.collider || !raycastHit2.collider || !raycastHit3.collider;
 
-        if (visible)
+            Debug.DrawLine(transform.position, mouseWorld);
+            Debug.DrawLine(transform.position, mouseWorld + perp);
+            Debug.DrawLine(transform.position, mouseWorld - perp);
+        }
+
+        follow &= dist < maxDistance;
+        follow &= dist > minDistance;
+
+        var rb = GetComponent<Rigidbody2D>();
+        if (follow)
         {
             Debug.DrawLine(transform.position, mouseWorld);
 
             dir = Quaternion.AngleAxis(followAngle, Vector3.forward) * dir;
 
-            var rb = GetComponent<Rigidbody2D>();
             rb.AddForce(dir * forceMagnitude * Time.deltaTime);
             //GetComponent<EnemyBase>().targetDir = dir;
         }
         else
         {
             //Debug.DrawLine(transform.position, raycastHit.point, Color.grey);
+            rb.AddForce(-rb.velocity * brakeForce * Time.deltaTime);
         }
     }
 }
